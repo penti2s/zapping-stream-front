bfeadc72830b2a3b852d7f6f45e38c7ff60a586a
Sergio Sepulveda Yañez <contacto@penti2s.com>
2024-01-21 12:23:01 -0300

    feat: template view for stream page

---
+++ pages/stream/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex flex-col items-center justify-center bg-base-200">
+    <header class="text-center mb-8 mt-5">
+      <h1 class="text-4xl font-bold text-base-content">Stream - 1</h1>
+    </header>
+    <main class="flex flex-1 w-full px-4">
+      <div class="w-full max-w-6xl mx-auto bg-base-100 shadow-lg rounded-lg p-4">
+        <div class=" w-full">
+          <stream-hsl />
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<style>
+
+</style>

7a47ea289620dd1fc0e48c889c9d83db56c4489a
Sergio Sepulveda Yañez <contacto@penti2s.com>
2024-01-21 18:25:12 -0300

    feat: main page without auth

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <template>
+  <header><h2>Bienvenido</h2></header>
     <div>
       <button
       class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
@@ -7,3 +8,12 @@
     </div>
 
 </template>
+
+
+<script setup lang="ts">
+import { definePageMeta } from '#imports'
+
+definePageMeta({
+  auth: false
+})
+</script>

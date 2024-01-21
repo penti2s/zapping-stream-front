12f48ffde5132c1335bfd6417ec4e25cd36f6e1a
Sergio Sepulveda Yañez <contacto@penti2s.com>
2024-01-21 18:25:57 -0300

    feat: protect route only user authenticate

--- pages/stream/index.vue
+++ pages/stream/index.vue
@@ -13,6 +13,11 @@
   </div>
 </template>
 
-<style>
+<script setup lang="ts">
+import { definePageMeta } from '#imports'
 
-</style>
+// Note: This is only for testing, it does not make sense to do this with `globalAppMiddleware` turned on
+definePageMeta({
+  middleware: 'auth'
+})
+</script>

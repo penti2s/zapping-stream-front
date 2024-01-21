098dd77f77c1fefac9f3dbd742ee782185eecfb4
Sergio Sepulveda Yañez <contacto@penti2s.com>
2024-01-20 23:08:36 -0300

    feat: define template, menu, login and register form

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <!-- suport light and dark mode in all app-->
+  <div  >
+    <div>
+      <header-menu />
+    </div>
+    <main class="bg-base-100">
+        <NuxtPage />
+    </main>
+  </div>
+</template>
---
+++ pages/auth/register.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="flex items-center justify-center min-h-screen bg-base-200">
+      <div class="py-6 mt-4 text-left bg-base-100 shadow-lg px-10 rounded-lg">
+        <h3 class="text-2xl font-bold text-center text-base-content">Registrarte</h3>
+        <form action="#">
+          <div class="mt-4">
+            <div>
+                <label class="block text-base-content" for="email">Nombre</label>
+                <input type="text" placeholder="Email" id="email"
+                  class="input input-bordered w-full max-w-xs">
+              </div>
+            <div>
+              <label class="block text-base-content" for="email">Email</label>
+              <input type="text" placeholder="Email" id="email"
+                class="input input-bordered w-full max-w-xs">
+            </div>
+            <div class="mt-4">
+              <label class="block text-base-content">Contraseña</label>
+              <input type="password" placeholder="Contraseña" id="password"
+                class="input input-bordered w-full max-w-xs">
+            </div>
+            <div class="flex flex-col items-center justify-between m-5">
+              <button class="btn btn-primary w-full max-w-xs mt-4">Registrarte</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </template>
+  
+  

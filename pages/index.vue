80a79eeb41e54f4b43b0617a5e8d78b20bc6fced
Sergio Sepulveda Yañez <contacto@penti2s.com>
2024-01-24 22:47:16 -0300

    feat: dynamic view for auth user

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import { definePageMeta } from '#imports'
 
+import { useAuth } from '#imports'
+
+const { signIn, token, data, status, signOut } = useAuth()
+
 definePageMeta({
   auth: false
 })
@@ -11,15 +15,20 @@ definePageMeta({
   <div class="flex flex-col items-center justify-center min-h-screen bg-base-200 text-base-content animate-fade-in-down">
     <header class="mb-8 text-center">
       <h2 class="text-5xl font-bold animate-fade-in">Bienvenido Cracks</h2>
-      <p class="mt-3 text-xl animate-fade-in-delay">Tu punto de partida para una experiencia increíble</p>
+      <p v-if="!data" class="mt-3 text-xl animate-fade-in-delay">Tu punto de partida para una experiencia increíble</p>
+      <p v-else class="mt-3 text-xl animate-fade-in-delay">¡No te pierdas nuestro stream en vivo!</p>
     </header>
-    <div class="flex flex-col items-center">
+    <div v-if="!data" class="flex flex-col items-center">
       <p class="text-lg animate-fade-in-delay">¿Ya tienes cuenta?</p>
-      <nuxt-link to="/auth/login"
-        class="btn btn-primary mt-4 animate-scale-up">
+      <nuxt-link to="/auth/login" class="btn btn-primary mt-4 animate-scale-up">
         Ingresar
       </nuxt-link>
       <p class="mt-6 text-lg animate-fade-in-delay">¿No tienes cuenta? <nuxt-link to="/auth/register" class="link link-hover">Regístrate aquí</nuxt-link></p>
+    </div>
+    <div v-else class="flex flex-col items-center">
+      <nuxt-link to="/stream" class="btn btn-primary mt-4 animate-scale-up">
+        Ver Stream
+      </nuxt-link>
     </div>
   </div>
 </template>
